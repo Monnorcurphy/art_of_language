8f5ed46549651751d703475b535452eca45c6947
ConnorLMurphy@gmail.com <connorlmurphy@gmail.com>
2017-02-26 21:35:15 -0800

    Add Liam Photo and Bio

--- art_of_language/frontend/components/about/about.jsx
+++ art_of_language/frontend/components/about/about.jsx
@@ -24,8 +24,22 @@ class About extends React.Component{
             <h2 className= 'information-title'>Liam Vogel-Spanish</h2>
             <div className='orange about-wrapper'>
               <div className='inner-div liam-about'>
-                <img src="assets/Moutains.jpg"/>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+                <img src="assets/Liam.jpeg"/>
+                <span className='personal'>
+              <p>My name is Liam Vogel and I'm a professional Spanish
+                  language teacher working out of Burbank.
+                  I just moved back from Argentina in South America
+                  and now I'm starting a private foreign language school.
+                  I previously studied and lived in Mexico, Guatemala,
+                  Colombia, and Argentina. </p>
+                  <p>I am a distinguished alumni
+                  of Mexico's Iberoamericana and la Universidad Católica
+                  de la Argentina. I also am about to finish my BA in
+                  Spanish Linguistics at Seattle University. Spanish is
+                  my passion I would love to help you understand it and
+                  teach you about the colorful cultures of Latin America.
+                  Call or text me to schedule a lesson.</p>
+              </span>
               </div>
             </div>
 
