46113720e5392f269eb16fe9ef82ea5209bf05fe
ConnorLMurphy@gmail.com <connorlmurphy@gmail.com>
2017-02-27 16:13:37 -0800

    Remove Bcrypt

--- art_of_language/app/models/user.rb
+++ art_of_language/app/models/user.rb
@@ -22,7 +22,7 @@ class User < ActiveRecord::Base
 	end
 
 	def password_is? password
-		BCrypt::Password.new(self.password_digest).is_password?(password)
+		# BCrypt::Password.new(self.password_digest).is_password?(password)
 	end
 
 	def reset_session_token!
